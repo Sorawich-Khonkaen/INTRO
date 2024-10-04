a77954bc99f7cfd616dab8b26a278e7afb1860da
Your Name <you@example.com>
2024-10-04 11:32:03 +0700

    HW

---
+++ E-commerce1/index.js
@@ -0,0 +1,281 @@
+var product = [{
+    id: 1,
+    img: 'https://www.resellzonecesena.com/cdn/shop/files/air-jordan-1-retro-high-og-heritage-555088-161-resellzone-1.jpg?v=1717597480',
+    name: 'Air Jordan 1',
+    price: 6500,
+
+    description: 'Air Jordan 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Sneakers'
+}, {
+    id: 2,
+    img: 'https://image.goxip.com/kfBvz7WAHXfKxeXJdul7kWXiurY=/fit-in/500x500/filters:format(jpg):quality(80):fill(white)/https:%2F%2Fcdn-images.farfetch-contents.com%2F18%2F97%2F81%2F82%2F18978182_41271609_1000.jpg',
+    name: 'Nike LeBron 19 ',
+    price: 7000,
+    description: 'Nike LeBron 19 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Sneakers'
+}, {
+    id: 3,
+    img: 'https://sneakers.id/images/products/FJ9488-100/0_2_1800x1800.jpg',
+    name: 'Nike KD 17 EP ',
+    price: 7300,
+    description: 'NIKE KD 17 EP Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Sneakers'
+},{
+    id: 4,
+    img: 'https://footpatrolblog.s3.amazonaws.com/wp-content/uploads/2020/11/Nike-Kobe-5-Bruce-Lee-IG-1.jpg',
+    name: 'Nike Kobe 5 ',
+    price: 30000,
+    description: 'Nike Kobe 5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Sneakers' 
+},{
+    id: 5,
+    img: 'https://media.architecturaldigest.com/photos/57a11cbeb6c434ab487bc26b/4:3/w_1032,h_774,c_limit/nikes-senior-designer-explains-what-went-into-new-air-jordan-01.png',
+    name: 'Air Jordan XXXI',
+    price: 4700,
+    description: 'Air Jordan XXXI Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Sneakers' 
+},{
+    id: 6,
+    img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/60a96da77cec4e389972dd01be7ba66c_faec/IN4247_HM1.jpg',
+    name: 'Adidas เสื้อวอร์มบาส',
+    price: 2500,
+    description: 'Adidas เสื้อวอร์มบาส Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Shirt' 
+},{
+    id: 7,
+    img: 'https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/6d5a0e0f-eb69-4836-8a9b-cbd3f386fab5/AS+M+NK+TEE+OS+OC.png',
+    name: 'เสื้อยืดบาสเก็ตบอลผู้ชาย Nike',
+    price: 1900,
+    description: 'เสื้อยืดบาสเก็ตบอลผู้ชาย NikeLorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Shirt' 
+},{
+    id: 8,
+    img: 'https://media.jdsports.com/i/jdsports/IN4244_020_M1?$default$&w=671&&h=671&bg=rgb(237,237,237)',
+    name: 'Adidas Basketball',
+    price: 1300,
+    description: 'Adidas BasketballLorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Shirt' 
+},{
+    id: 9,
+    img: 'https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/21a5a90b-716c-418f-9720-915322ab9fb8/AS+M+NK+DF+TEE+RLGD+OC+ICON.png',
+    name: 'เสื้อยืดบาสเก็ตบอลผู้ชาย Dri-FIT Nike',
+    price: 1100,
+    description: 'เสื้อยืดบาสเก็ตบอลผู้ชาย Dri-FIT Nike Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Shirt' 
+},{
+    id: 10,
+    img: 'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/538564/01/mod01/fnd/THA/fmt/png/%E0%B9%80%E0%B8%AA%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B8%A2%E0%B8%B7%E0%B8%94%E0%B8%9A%E0%B8%B2%E0%B8%AA%E0%B9%80%E0%B8%81%E0%B9%87%E0%B8%95%E0%B8%9A%E0%B8%AD%E0%B8%A5%E0%B8%8A%E0%B8%B2%E0%B8%A2-Technical',
+    name: 'เสื้อยืดบาสเก็ตบอลชาย Technical',
+    price: 1050,
+    description: 'เสื้อยืดบาสเก็ตบอลชาย Technical Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Shirt' 
+},{
+    id: 11,
+    img: 'https://www.cpschaps.com/media/catalog/product/c/2/c24wmqa914_red_6.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=978&width=700&canvas=700:978',
+    name: 'Basketball Loose pants',
+    price: 1890,
+    description: 'Basketball Loose pants Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Trousers' 
+},{
+    id: 12,
+    img: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/3c7f94b5259b4fb1a93baf710001e9fe_9366/Basketball_Warm-Up_Pants_Green_IK8559_23_hover_model.jpg',
+    name: 'Basketball Warm-Up Pants',
+    price: 1440,
+    description: 'Basketball Warm-Up Pants Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Trousers' 
+},{
+    id: 13,
+    img: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/622079/02/mod01/fnd/IDN/fmt/png/Blueprint-Formstrip-Men's-Basketball-Pants",
+    name: 'Basketball Pants | PUMA',
+    price: 1200,
+    description: 'Basketball Pants | PUMA Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?',
+    type: 'Trousers'
+},{
+    id: 14,
+    img: "https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/4a13d3fd-1247-4e0e-b077-f12e3c859cc6/M+NK+STRTFV+WVN+PANT.png",
+    name: "Nike Starting 5 Men's Basketball Pants",
+    price: 1500,
+    description: "Nike Starting 5 Men's Basketball Pants Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?",
+    type: 'Trousers'
+},{
+    id: 15,
+    img: "https://midwaysports.com/cdn/shop/products/CW7351-010-PHSFM001.jpg?v=1633546581",
+    name: "Nike Dri-Fit Men's Basketball Pants",
+    price: 1200,
+    description: "Nike Dri-Fit Men's Basketball Pants Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit assumenda eos quasi, eaque expedita?",
+    type: 'Trousers'
+}]
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openproductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p style="font-size: 0.9vw;">${numberWithCommas(product[i].price)} THB</p>
+                </div>`;
+    }
+    $("#productlist").html(html);
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchtxt(elem) {
+    //console.log('#'+elem.id)
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if(product[i].name.includes(value)) {
+            html += `<div onclick="openproductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p style="font-size: 0.9vw;">${numberWithCommas(product[i].price)} THB</p>
+                </div>`;
+        }
+        
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if(param =='all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("."+param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openproductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name);
+    $("#mdd-price").text(numberWithCommas(product[index].price) + ' THB');
+    $("#mdd-desc").text(product[index].description);
+}
+
+function closeModal() {
+    $(".modal").css('display', 'none')
+}
+
+var cart = [];
+function addtocaet() {
+    var pass = true;
+
+    for (let i = 0; i< cart.length; i++) {
+        if(productindex == cart[i].index) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+
+        }
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+       // console.log(obj)
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add' + product[productindex].name + ' to cart !'
+    })
+    $('#cartcount').css('display', 'flex').text(cart.length)
+}
+
+function openCart() {
+    $('#modalCart').css('display', 'flex')
+    rendercart();
+}
+
+function rendercart() {
+    if(cart.length > 0) {
+        var html =  '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlist-items">
+                        <div class="cartlist-left">
+                            <img src="${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
+                                <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                                <p style="font-size: 1.2vw;">${numberWithCommas(cart[i].price * cart[i].count)} THB</p>
+                            </div>
+                        </div>
+                        <div class="cartlist-right">
+                            <p onclick="deinitems('-',${i})" class="btnc">-</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+',${i})" class="btnc">+</p>
+                        </div>
+                    </div>`;
+       }
+       $("#mycart").html(html)
+
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+}
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems" + index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    shpwCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    shpwCancelButtonText: 'Cancel'
+                }).then((res) => {
+                    if(res.isConfirmed) {
+                        cart.splice(index, 1)
+                        console.log(cart)
+                        rendercart();
+                        $('#cartcount').css('display', 'flex').text(cart.length)
+
+                        if(cart.length <= 0) {
+                            $('#cartcount').css('display', 'none')
+                        }
+                    }
+                    else {
+                        cart[index].count++;
+                        $('#countitems'+index).text(cart[index].count)
+                    }
+                })
+            }
+        }
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $('#countitems'+index).text(cart[index].count)
+    }
+}
